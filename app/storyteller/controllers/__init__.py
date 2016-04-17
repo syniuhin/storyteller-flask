8b949246b8e3ad5ea29440c76aefe223faa485e4
infm <syniuhin@gmail.com>
2016-04-17 12:34:15 +0300

    Changed mock model to a real one.

--- app/storyteller/controllers/__init__.py
+++ app/storyteller/controllers/__init__.py
@@ -1,7 +1,7 @@
-from app.storyteller.core import MockGenerator
-
 from flask import Blueprint
 
-story_model = MockGenerator()  # StoryModel()
+from app.storyteller.core import StoryGenerator
+
+story_model = StoryGenerator()
 
 storyteller = Blueprint('storyteller', __name__, url_prefix='/storyteller')
